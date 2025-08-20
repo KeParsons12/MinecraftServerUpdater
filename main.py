b99c6df7e3322c7fa7c3d9d8024df36c81b65fcc
Kurtis <kparsons24@gmail.com>
2025-08-20 14:29:48 -0400

    Make startServer and main into async functions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import requests, shutil, datetime, os, subprocess, time, platform
+import requests, shutil, datetime, os, subprocess, time, platform, asyncio
 
 def readConfig():
     print('Reading config...')
@@ -91,15 +91,16 @@ def downloadLatestServer():
                 open(config['server-directory'] + '/server.jar', 'wb').write(requests.get(serverUrl).content)
                 print('Download complete.')
 
-def startServer(firstRun=False):
+async def startServer(firstRun=False):
     print('Starting server...')
     os.chdir(config['server-directory'])
     if osName == 'Linux':
-        cmd = subprocess.Popen('java ' + '-Xms1024M -Xmx' + config['dedicated-ram'] + 'M ' + '-jar ' + config['server-directory'] + '/server.jar --nogui', creationflags=subprocess.CREATE_NEW_CONSOLE, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        cmd = await asyncio.create_subprocess_shell('java ' + '-Xms1024M -Xmx' + config['dedicated-ram'] + 'M ' + '-jar ' + config['server-directory'] + '/server.jar --nogui', creationflags=subprocess.CREATE_NEW_CONSOLE, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
     elif osName == 'Windows':
-        cmd = subprocess.Popen('java ' + '-Xms1024M -Xmx' + config['dedicated-ram'] + 'M ' + '-jar ' + config['server-directory'] + '/server.jar --nogui', creationflags=subprocess.CREATE_NEW_CONSOLE, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+        cmd = await asyncio.create_subprocess_shell('java ' + '-Xms1024M -Xmx' + config['dedicated-ram'] + 'M ' + '-jar ' + config['server-directory'] + '/server.jar --nogui', creationflags=subprocess.CREATE_NEW_CONSOLE, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
         if firstRun:
             while os.path.exists(config['server-directory'] + '/eula.txt') == False:
+                cmd.communicate()
                 time.sleep(5)
             time.sleep(10)
             cmd.terminate()
@@ -107,135 +108,181 @@ def startServer(firstRun=False):
     os.chdir(currentDirectory)
     return cmd
 
+async def printCmdOutput(cmd):
+    return await cmd.stdout.readline()
+
+# async def startServer(firstRun=False):
+#     print('Starting server...')
+#     os.chdir(config['server-directory'])
+#     if osName == 'Linux':
+#         cmd = await asyncio.create_subprocess_shell('java ' + '-Xms1024M -Xmx' + config['dedicated-ram'] + 'M ' + '-jar ' + config['server-directory'] + '/server.jar --nogui', creationflags=asyncio.subprocess.CREATE_NEW_CONSOLE, stdin=asyncio.subprocess.PIPE, stdout=asyncio.subprocess.PIPE)
+#     elif osName == 'Windows':
+#         cmd = await asyncio.create_subprocess_shell('java ' + '-Xms1024M -Xmx' + config['dedicated-ram'] + 'M ' + '-jar ' + config['server-directory'] + '/server.jar --nogui', stdin=asyncio.subprocess.PIPE, stdout=asyncio.subprocess.PIPE)
+        
+#     if firstRun:
+#         while os.path.exists(config['server-directory'] + '/eula.txt') == False:
+#             # cmd.communicate()
+#             time.sleep(5)
+#         time.sleep(10)
+#         cmd.terminate()
+#         cmd.wait()
+#     os.chdir(currentDirectory)
+#     return cmd
+
+def backupSever():
+    print('Backing up server...')
+    if os.path.exists(config['server-directory']):
+
+    # Create backup of server folder
+        shutil.copytree(config['server-directory'], config['server-directory'] + datetime.date.today().strftime("%Y%m%d"), dirs_exist_ok=True)
+
+    else:
+        raise FileNotFoundError(f"Backup failed: No such directory. {config['server-directory']}") 
+    return
+
 osName = platform.system()
 currentDirectory = os.path.abspath(os.path.curdir)
 
-try:
+# Read config file or create one with default values if one does not exist.
+config = readConfig()
 
-    # Read config file or create one with default values if one does not exist.
-    config = readConfig()
+async def main():
+    try:
 
-    # Read Eula file and mark true if exists. If not start server to generate eula.
-    if os.path.exists(config['server-directory'] + '/eula.txt') & os.path.exists(config['server-directory'] + '/server.jar'):
-        checkEula()
-        writeConfig()
+        # Read Eula file and mark true if exists. If not start server to generate eula.
+        if os.path.exists(config['server-directory'] + '/eula.txt') & os.path.exists(config['server-directory'] + '/server.jar'):
+            checkEula()
+            writeConfig()
 
-    elif os.path.exists(config['server-directory'] + '/server.jar'):
-        startServer(True)
-        writeConfig()
-        checkEula()
-
-    else:
-        downloadLatestServer()
-        startServer(True)
-        writeConfig()
-        checkEula()
-
-    gameWindow = startServer()
-
-
-
-    serverDirectory = "/home/kupar/gameservers/minecraft"
-    # currentDirectory = "/home/kupar/gameservers"
-    logFilePath = "/home/kupar/gameservers/log.txt"
-    # currentTime = datetime.datetime.now()   
-    # laterTime = currentTime + datetime.timedelta(hours = 1)
-    # laterTimeSecond = currentTime + datetime.timedelta(seconds = 1)
-
-    # while True:
-    # while currentTime < laterTime:
-    #     currentTime = datetime.datetime.now()
-        
-        # if currentTime >= laterTimeSecond:
-        #     print(currentTime)
-        #     laterTimeSecond = currentTime + datetime.timedelta(seconds = 1)
-
-
-    # laterTime = currentTime + datetime.timedelta(hours = 1)
-    # gameWindow.stdout.flush()
-    # time.sleep(25)
-    # stopcmd = '/stop\n'
-    # gameWindow.stdin.write(stopcmd.encode("utf-8"))
-    # gameWindow.stdin.flush()
-    # supercool = gameWindow.stdout.readlines()
-    # print(supercool)
-    print('script finished')
-    exit(0)
-
-except Exception as e:
-    print(e)
-    exit(1)
-
-try:
-    # Get status of minecraft server
-    response = requests.get('https://api.mcstatus.io/v2/status/java/kurtisparsons.com')
-    print('Server status response code:', response.status_code)
-    if response.status_code == 200:
-
-        responseJson = response.json()
-
-        serverVersion = responseJson['version']['name_raw']
-        playerCount = responseJson['players']['online']
-
-        if playerCount == 0:
-            # Get latest version number
-            response = requests.get('https://launchermeta.mojang.com/mc/game/version_manifest.json')
-            print('Version manifest response code:', response.status_code)
-            if response.status_code == 200:
-                responseJson = response.json()
-
-                latestVersion = responseJson['latest']['release']
-
-                # print(len(responseJson['versions']))
-
-                if serverVersion != latestVersion:
-                    for versions in responseJson['versions']:
-                        if versions['id'] == latestVersion:
-                            # print(versions['url'])
-                            # Get Url to download the latest server .jar file
-                            response = requests.get(versions['url'])
-                            print('download link response code:', response.status_code)
-                            if response.status_code == 200:
-                                responseJson = response.json()
-                                serverUrl = responseJson['downloads']['server']['url']
-                                # print(serverUrl)
-                                # Download latest .jar file
-                                open(currentDirectory + '/server.jar', 'wb').write(requests.get(serverUrl).content)
-                                print('Download complete.')
-
-                                # Bash Commands (stop server process) (wait for process to stop)
-                                bashResult = subprocess.run("systemctl stop minecraft-server.service", shell=True, capture_output=True, text=True)
-                                print(bashResult)
-                                time.sleep(30)
-
-                                # Copy server folder
-                                if os.path.exists(serverDirectory):
-                                    # print(serverDirectory, datetime.date.today())
-                                    # Create backup of server folder
-                                    shutil.copytree(serverDirectory, serverDirectory + datetime.date.today().strftime("%Y%m%d"), dirs_exist_ok=True)
-                                    
-                                    # Move new server file into existing directory
-                                    shutil.copy(currentDirectory + '/server.jar', serverDirectory)
-
-                                    # Bash commands (start server process)
-                                    bashResult = subprocess.run("systemctl start minecraft-server.service", shell=True, capture_output=True, text=True)
-                                    print(bashResult)
-                                    open(logFilePath, 'a').write(f'{datetime.datetime.now()}: Server succesfully updated to version: {latestVersion}\n')
-                                    
-                                else:
-                                    print('Server directory does not exist.')
-                                    open(logFilePath, 'a').write(f'{datetime.datetime.now()}: Server directory does not exist.\n')                               
-
-                else:
-                    print(f'Server is up to date. Version: {serverVersion}')
-                    open(logFilePath, 'a').write(f'{datetime.datetime.now()}: Server is up to date. Version: {serverVersion}.\n')
+        elif os.path.exists(config['server-directory'] + '/server.jar'):
+            await startServer(True)
+            writeConfig()
+            checkEula()
 
         else:
-            print('Server not empty.', playerCount, 'players in game.')
-            open(logFilePath, 'a').write(f'{datetime.datetime.now()}: Server not empty. {playerCount} players in game.\n')
+            downloadLatestServer()
+            await startServer(True)
+            writeConfig()
+            checkEula()
+
+        gameWindow = await startServer()
+
+        gameWindowPrinter = asyncio.create_task(gameWindow.stdout.readline())
+
+        while True:
+            if gameWindowPrinter.done():
+                print(gameWindowPrinter.result())
+                gameWindowPrinter = asyncio.create_task(printCmdOutput(gameWindow))
+            #await asyncio.sleep(0.1)
+            await print('Weeeee')
 
 
-except Exception as e:
-    print('Unspecified Error')
-    print(e)
-    open(logFilePath, 'a').write(f'{datetime.datetime.now()}: ERROR: {e}\n')
+
+
+
+        serverDirectory = "/home/kupar/gameservers/minecraft"
+        # currentDirectory = "/home/kupar/gameservers"
+        logFilePath = "/home/kupar/gameservers/log.txt"
+        # currentTime = datetime.datetime.now()   
+        # laterTime = currentTime + datetime.timedelta(hours = 1)
+        # laterTimeSecond = currentTime + datetime.timedelta(seconds = 1)
+
+        # while True:
+        # while currentTime < laterTime:
+        #     currentTime = datetime.datetime.now()
+            
+            # if currentTime >= laterTimeSecond:
+            #     print(currentTime)
+            #     laterTimeSecond = currentTime + datetime.timedelta(seconds = 1)
+
+
+        # laterTime = currentTime + datetime.timedelta(hours = 1)
+        # gameWindow.stdout.flush()
+        # time.sleep(25)
+        # stopcmd = '/stop\n'
+        # gameWindow.stdin.write(stopcmd.encode("utf-8"))
+        # gameWindow.stdin.flush()
+        # supercool = gameWindow.stdout.readlines()
+        # print(supercool)
+        print('script finished')
+        exit(0)
+
+    except Exception as e:
+        print(e)
+        exit(1)
+
+asyncio.run(main())
+
+# try:
+#     # Get status of minecraft server
+#     response = requests.get('https://api.mcstatus.io/v2/status/java/kurtisparsons.com')
+#     print('Server status response code:', response.status_code)
+#     if response.status_code == 200:
+
+#         responseJson = response.json()
+
+#         serverVersion = responseJson['version']['name_raw']
+#         playerCount = responseJson['players']['online']
+
+#         if playerCount == 0:
+#             # Get latest version number
+#             response = requests.get('https://launchermeta.mojang.com/mc/game/version_manifest.json')
+#             print('Version manifest response code:', response.status_code)
+#             if response.status_code == 200:
+#                 responseJson = response.json()
+
+#                 latestVersion = responseJson['latest']['release']
+
+#                 # print(len(responseJson['versions']))
+
+#                 if serverVersion != latestVersion:
+#                     for versions in responseJson['versions']:
+#                         if versions['id'] == latestVersion:
+#                             # print(versions['url'])
+#                             # Get Url to download the latest server .jar file
+#                             response = requests.get(versions['url'])
+#                             print('download link response code:', response.status_code)
+#                             if response.status_code == 200:
+#                                 responseJson = response.json()
+#                                 serverUrl = responseJson['downloads']['server']['url']
+#                                 # print(serverUrl)
+#                                 # Download latest .jar file
+#                                 open(currentDirectory + '/server.jar', 'wb').write(requests.get(serverUrl).content)
+#                                 print('Download complete.')
+
+#                                 # Bash Commands (stop server process) (wait for process to stop)
+#                                 bashResult = subprocess.run("systemctl stop minecraft-server.service", shell=True, capture_output=True, text=True)
+#                                 print(bashResult)
+#                                 time.sleep(30)
+
+#                                 # Copy server folder
+#                                 if os.path.exists(serverDirectory):
+#                                     # print(serverDirectory, datetime.date.today())
+#                                     # Create backup of server folder
+#                                     shutil.copytree(serverDirectory, serverDirectory + datetime.date.today().strftime("%Y%m%d"), dirs_exist_ok=True)
+                                    
+#                                     # Move new server file into existing directory
+#                                     shutil.copy(currentDirectory + '/server.jar', serverDirectory)
+
+#                                     # Bash commands (start server process)
+#                                     bashResult = subprocess.run("systemctl start minecraft-server.service", shell=True, capture_output=True, text=True)
+#                                     print(bashResult)
+#                                     open(logFilePath, 'a').write(f'{datetime.datetime.now()}: Server succesfully updated to version: {latestVersion}\n')
+                                    
+#                                 else:
+#                                     print('Server directory does not exist.')
+#                                     open(logFilePath, 'a').write(f'{datetime.datetime.now()}: Server directory does not exist.\n')                               
+
+#                 else:
+#                     print(f'Server is up to date. Version: {serverVersion}')
+#                     open(logFilePath, 'a').write(f'{datetime.datetime.now()}: Server is up to date. Version: {serverVersion}.\n')
+
+#         else:
+#             print('Server not empty.', playerCount, 'players in game.')
+#             open(logFilePath, 'a').write(f'{datetime.datetime.now()}: Server not empty. {playerCount} players in game.\n')
+
+
+# except Exception as e:
+#     print('Unspecified Error')
+#     print(e)
+#     open(logFilePath, 'a').write(f'{datetime.datetime.now()}: ERROR: {e}\n')
