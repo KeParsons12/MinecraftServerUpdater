5e5db100b48bf3f3cced74298b9e0059438bdbdb
Keenan Parsons <keenparsons@gmail.com>
2025-08-28 21:02:37 -0400

    feat: removed unused GUI lines

--- gui.py
+++ gui.py
@@ -27,9 +27,7 @@ class GuiWindow:
 
     def layoutWindowWidgets(self):
         # Configure rows/columns expanding
-        self.root.rowconfigure(0, weight=1)
-        self.root.rowconfigure(1, weight=0) # button row stays fixed
-        self.root.columnconfigure(0, weight=0)
+        self.root.rowconfigure(0, weight=1) # ID, Should widget resize: 0 = no resize, 1 = resize
         self.root.columnconfigure(1, weight=1)
 
         self.titleLabel.grid(row=0, column=0)
